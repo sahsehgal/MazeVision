9b18270c8755292c73e8964968939242079e74f4
Sahil Sehgal <sahilsehgal@sahils-mbp.home>
2020-12-14 20:28:21 -0500

    customize graph level

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from helper import randomize_maze_board, get_level_statistics
 from game import start_game
 from q_learning import start_qlearning_game
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
 def get_level_input():
         print("Enter level number you want to play. ")
@@ -26,6 +28,24 @@ def get_control_strategy():
         print("3. Tree-based AI")
         print("4. Tree + NN based AI")
         return int(input())                        
+
+def plot_graph(data, key, title, file_name):
+        # data = [data['baseline_AI'], data['treebase_AI'], data['nn_tree_AI']]
+        labels = ["{}".format(i+1) for i in range(len(data['baseline_AI']))]
+        X = data['baseline_AI']
+        Y = data['treebase_AI']
+        Z = data['nn_tree_AI']
+
+        df = pd.DataFrame(np.c_[X,Y,Z], index=labels)
+        df.plot.bar()
+        y_pos = range(len(labels))
+        plt.xticks(y_pos, labels, rotation=0)
+        plt.title(title, fontsize=14)
+        plt.xlabel('Game Instances', fontsize=14)
+        plt.ylabel(key, fontsize=14)
+        plt.savefig(file_name)
+        # plt.show()
+        
 
 class Main:
         def play_game(self):
@@ -49,11 +69,34 @@ class Main:
                         start_game(active_maze, strategy)
 
         def get_statistics(self):
+                print("Enter number of instances per level.")
+                no_of_instances = int(input())
                 for level in range(1,6):
-                        for instance in range(100):
+                        scores, neurons = {}, {}
+                        scores['baseline_AI'] = []
+                        scores['treebase_AI'] = []
+                        scores['nn_tree_AI'] = []
+                        
+                        neurons['baseline_AI'] = []
+                        neurons['treebase_AI'] = []
+                        neurons['nn_tree_AI'] = []
+                        for instance in range(no_of_instances):
                                 active_maze = randomize_maze_board(get_maze(level))
                                 print ("Level: {}, Instance: {}".format(level, instance+1))
-                                print ("Statistics: {}\n".format(get_level_statistics(active_maze, level)))
+                                statistics = get_level_statistics(active_maze, level)
+                                print ("Statistics: {}\n".format(statistics))
+                                
+                                scores['baseline_AI'].append(statistics['baseline_AI']['score'])
+                                scores['treebase_AI'].append(statistics['treebase_AI']['score'])
+                                scores['nn_tree_AI'].append(statistics['nn_tree_AI']['score'])
+
+                                neurons['baseline_AI'].append(statistics['baseline_AI']['no_of_nodes_traversed'])
+                                neurons['treebase_AI'].append(statistics['treebase_AI']['no_of_nodes_traversed'])
+                                neurons['nn_tree_AI'].append(statistics['nn_tree_AI']['no_of_nodes_traversed'])
+                        
+                        plot_graph(scores, 'Scores', 'Scores of Level {}'.format(level), 'Level_{}_scores.png'.format(level))
+                        plot_graph(neurons, 'Neurons', 'No. of nodes/neurons traversed in Level {}'.format(level), 'Level_{}_neurons.png'.format(level))
+
 
 if __name__ == "__main__":
         maze = Main()
